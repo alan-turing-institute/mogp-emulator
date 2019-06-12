fc538bcd216f6e084be3d58a051fe403dec75288
Eric Daub <edaub@turing.ac.uk>
2019-06-12 16:34:57 +0100

    have most basic functionality of sequential design working with unit tests

--- mogp_emulator/SequentialDesign.py
+++ mogp_emulator/SequentialDesign.py
@@ -4,7 +4,7 @@ from .ExperimentalDesign import ExperimentalDesign
 
 class SequentialDesign(object):
     "Base class for a sequential experimental design"
-    def __init__(self, base_design, f, n_samples = None, n_init = 10, n_cand = 50, nugget = 1.):
+    def __init__(self, base_design, f, n_targets = 1, n_samples = None, n_init = 10, n_cand = 50, nugget = 1.):
         "create new instance of an experimental design"
         
         if not isinstance(base_design, ExperimentalDesign):
@@ -15,6 +15,9 @@ class SequentialDesign(object):
             
         if not len(signature(f).parameters) == 1:
             raise ValueError("simulator f must accept all parameters as a single input array")
+        
+        if int(n_targets) < 0:
+            raise ValueError("number of targets must be positive")
         
         if (not n_samples == None) and int(n_samples) <= 0:
             raise ValueError("number of samples must be positive")
@@ -33,6 +36,7 @@ class SequentialDesign(object):
         
         self.base_design = base_design
         self.f = f
+        self.n_targets = int(n_targets)
         if n_samples == None:
             self.n_samples = None
         else:
@@ -47,65 +51,144 @@ class SequentialDesign(object):
         self.targets = None
         self.candidates = None
     
+    def get_n_targets(self):
+        "get number of targets in design"
+        
+        return self.n_targets
+    
     def get_n_parameters(self):
         "get number of parameters in design"
+        
         return self.base_design.get_n_parameters()
     
     def get_n_init(self):
         "get number of initial design points"
+        
         return self.n_init
         
     def get_n_samples(self):
         "get total number of samples"
+        
         return self.n_samples
     
     def get_n_cand(self):
         "get number of candidate points"
+        
         return self.n_cand
         
     def get_nugget(self):
         "get nugget parameter for smoothing predictions"
+        
         return self.nugget
         
     def get_current_iteration(self):
         "get current iteration"
+        
         return self.current_iteration
         
     def get_inputs(self):
         "get current design"
+        
         return self.inputs
         
     def get_targets(self):
         "get current value of targets"
+        
         return self.targets
         
     def get_candidates(self):
         "get current value of candidates"
+        
         return self.candidates
         
     def get_base_design(self):
         "get type of base design"
+        
         return type(self.base_design).__name__
     
     def generate_initial_design(self):
         "create initial design"
-        pass
+        
+        self.inputs = self.base_design.sample(self.n_init)
+        self.current_iteration = self.n_init
+        return self.inputs
+    
+    def set_initial_targets(self, targets):
+        "set initial targets"
+        
+        if self.inputs is None:
+            raise ValueError("Initial design has not been generated")
+        else:
+            assert self.inputs.shape == (self.n_init, self.get_n_parameters()), "inputs have not been initialized correctly"
+        
+        if len(np.array(targets).shape) == 1:
+            targets = np.reshape(np.array(targets), (1, len(np.array(targets))))
+            
+        assert np.array(targets).shape == (self.n_targets, self.n_init), "initial targets must have shape (n_targets, n_init)"
+        
+        self.targets = np.array(targets)
+        self.initialized = True
     
     def run_init_design(self):
         "run initial design"
-        pass
+        
+        inputs = self.generate_initial_design()
+        targets = np.full((self.n_targets, self.n_init), np.nan)
+        
+        for i in range(self.n_init):
+            targets[:,i] = np.array(self.f(inputs[i,:]))
+            
+        assert np.all(np.isfinite(targets)), "error in initializing sequential design, function outputs may not be the correct shape"
+        self.set_initial_targets(targets)
         
     def _generate_candidates(self):
         "generate candidates for next iteration"
-        pass
+        
+        self.candidates = self.base_design.sample(self.n_cand)
     
-    def _eval_criterion(self):
-        "evaluate criterion for selecting next point on candidates"
-        pass
+    def _eval_metric(self):
+        "evaluate metric for selecting next point on candidates"
+        raise NotImplementedError("Base class for Sequential Design does not implement an evaluation metric")
     
     def get_next_point(self):
         "evaluate candidates to determine next point"
-        pass
+        
+        self._generate_candidates()
+        next_index = self._eval_metric()
+        
+        new_inputs = np.empty((self.current_iteration + 1, self.get_n_parameters()))
+        new_inputs[:-1, :] = self.inputs
+        
+        next_point = self.candidates[next_index,:]
+        new_inputs[-1,:] = next_point
+        
+        self.inputs = np.array(new_inputs)
+        
+        return next_point
+    
+    def set_next_target(self, target):
+        "set value of next target"
+        
+        if self.inputs is None:
+            raise ValueError("Initial design has not been generated")
+        else:
+            assert self.inputs.shape == (self.current_iteration + 1, self.get_n_parameters()), "inputs have not been correctly updated"
+        
+        if self.targets is None:
+            raise ValueError("Initial targets have not been generated")
+        else:
+            assert self.targets.shape == (self.n_targets, self.current_iteration), "targets have not been correctly updated"
+        
+        if isinstance(target, float):
+            target = np.reshape(np.array(target), (1,))
+        assert target.shape == (self.n_targets,), "new target must have shape (n_targets,)"
+        
+        new_targets = np.empty((self.n_targets, self.current_iteration + 1))
+        new_targets[:,:-1] = self.targets
+        new_targets[:,-1] = np.array(target)
+        
+        self.targets = np.array(new_targets)
+        self.current_iteration = self.current_iteration + 1
     
     def run_sequential_design(self, n_samples = None):
         "run the entire sequential design"
--- mogp_emulator/tests/test_SequentialDesign.py
+++ mogp_emulator/tests/test_SequentialDesign.py
@@ -18,6 +18,7 @@ def test_SequentialDesign_init():
     assert type(sd.base_design).__name__ == 'LatinHypercubeDesign'
     assert callable(sd.f)
     assert len(signature(sd.f).parameters) == 1
+    assert sd.n_targets == 1
     assert sd.n_samples == None
     assert sd.n_init == 10
     assert sd.n_cand == 50
@@ -28,10 +29,11 @@ def test_SequentialDesign_init():
     assert sd.targets == None
     assert sd.candidates == None
     
-    sd = SequentialDesign(ed, f, n_samples = 100, n_init = 20, n_cand = 100, nugget = 0.1)
+    sd = SequentialDesign(ed, f, n_targets = 5, n_samples = 100, n_init = 20, n_cand = 100, nugget = 0.1)
     assert type(sd.base_design).__name__ == 'LatinHypercubeDesign'
     assert callable(sd.f)
     assert len(signature(sd.f).parameters) == 1
+    assert sd.n_targets == 5
     assert sd.n_samples == 100
     assert sd.n_init == 20
     assert sd.n_cand == 100
@@ -63,6 +65,9 @@ def test_SequentialDesign_init_failures():
         sd = SequentialDesign(ed, f2)
         
     with pytest.raises(ValueError):
+        sd = SequentialDesign(ed, f, n_targets = -1)
+           
+    with pytest.raises(ValueError):
         sd = SequentialDesign(ed, f, n_samples = -1)
         
     with pytest.raises(ValueError):
@@ -77,6 +82,17 @@ def test_SequentialDesign_init_failures():
     with pytest.raises(ValueError):
         sd = SequentialDesign(ed, f, nugget = -1.)
         
+def test_SequentialDesign_get_n_targets():
+    "test the get_n_targets method"
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.array([1.])
+        
+    sd = SequentialDesign(ed, f, n_targets = 3)
+    assert sd.get_n_targets() == 3
+
 def test_SequentialDesign_get_n_parameters():
     "test the get_n_parameters method"
     
@@ -198,3 +214,224 @@ def test_SequentialDesign_get_base_design():
         
     sd = SequentialDesign(ed, f)
     assert sd.get_base_design() == "LatinHypercubeDesign"
+    
+def test_SequentialDesign_generate_initial_design():
+    "test the generate_initial_design method"
+    
+    np.random.seed(74632)
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.array([1.])
+        
+    sd = SequentialDesign(ed, f, n_init = 4)
+    initial_design_expected = np.array([[0.9660431763890672, 0.2080126306969736, 0.2576380063570568],
+                                        [0.0684779421445063, 0.9308367720360009, 0.1428493015158686],
+                                        [0.6345029195085983, 0.6651343562344474, 0.8827198350687029],
+                                        [0.4531112960399023, 0.3977273628763245, 0.5867585643640021]])
+    initial_design_actual = sd.generate_initial_design()
+    assert_allclose(initial_design_actual, initial_design_expected)
+    assert_allclose(sd.inputs, initial_design_expected)
+    assert sd.current_iteration == 4
+    
+def test_SequentialDesign_set_initial_targets():
+    "test the set_initial_targets method"
+    
+    np.random.seed(74632)
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.sum(x)
+        
+    sd = SequentialDesign(ed, f, n_init = 4)
+    initial_design_expected = np.array([[0.9660431763890672, 0.2080126306969736, 0.2576380063570568],
+                                        [0.0684779421445063, 0.9308367720360009, 0.1428493015158686],
+                                        [0.6345029195085983, 0.6651343562344474, 0.8827198350687029],
+                                        [0.4531112960399023, 0.3977273628763245, 0.5867585643640021]])
+    targets_expected = np.array([np.sum(i) for i in initial_design_expected])
+    sd.generate_initial_design()
+    sd.set_initial_targets(targets_expected)
+    assert_allclose(sd.targets, np.reshape(targets_expected, (1,4)))
+    assert sd.initialized
+    
+    sd = SequentialDesign(ed, f, n_init = 4)
+    targets_expected = np.array([[np.sum(i) for i in initial_design_expected]])
+    sd.generate_initial_design()
+    sd.set_initial_targets(targets_expected)
+    assert_allclose(sd.targets, targets_expected)
+    assert sd.initialized
+    
+    sd = SequentialDesign(ed, f, n_init = 4)
+    with pytest.raises(ValueError):
+        sd.set_initial_targets(targets_expected)
+        
+    sd = SequentialDesign(ed, f, n_init = 4)
+    sd.generate_initial_design()
+    with pytest.raises(AssertionError):
+        sd.set_initial_targets(np.zeros((2, 4)))
+        
+    sd = SequentialDesign(ed, f, n_init = 4)
+    sd.inputs = np.zeros((5,2))
+    with pytest.raises(AssertionError):
+        sd.set_initial_targets(targets_expected)
+        
+def test_SequentialDesign_run_init_design():
+    "test method to run initial design"
+    
+    np.random.seed(74632)
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.sum(x)
+        
+    sd = SequentialDesign(ed, f, n_init = 4)
+    initial_design_expected = np.array([[0.9660431763890672, 0.2080126306969736, 0.2576380063570568],
+                                        [0.0684779421445063, 0.9308367720360009, 0.1428493015158686],
+                                        [0.6345029195085983, 0.6651343562344474, 0.8827198350687029],
+                                        [0.4531112960399023, 0.3977273628763245, 0.5867585643640021]])
+    targets_expected = np.array([[np.sum(i) for i in initial_design_expected]])
+    sd.run_init_design()
+    assert_allclose(sd.inputs, initial_design_expected)
+    assert_allclose(sd.targets, targets_expected)
+    assert sd.initialized
+    assert sd.current_iteration == 4
+    
+def test_SequentialDesign_generate_candidates():
+    "test the _generate_candidates method"
+    
+    np.random.seed(74632)
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.sum(x)
+        
+    sd = SequentialDesign(ed, f, n_cand = 4)
+    candidates_expected = np.array([[0.9660431763890672, 0.2080126306969736, 0.2576380063570568],
+                                    [0.0684779421445063, 0.9308367720360009, 0.1428493015158686],
+                                    [0.6345029195085983, 0.6651343562344474, 0.8827198350687029],
+                                    [0.4531112960399023, 0.3977273628763245, 0.5867585643640021]])
+    sd._generate_candidates()
+    assert_allclose(sd.candidates, candidates_expected)
+    
+def test_SequentialDesign_eval_metric():
+    "test the _eval_metric method"
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.sum(x)
+        
+    sd = SequentialDesign(ed, f)
+    
+    with pytest.raises(NotImplementedError):
+        sd._eval_metric()
+        
+def test_SequentialDesign_get_next_point():
+    "test the get_next_point method"
+    
+    np.random.seed(74632)
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.sum(x)
+        
+    def tmp_eval_metric(self):
+        return 0
+        
+    sd = SequentialDesign(ed, f, n_init = 4, n_cand = 4)
+    
+    sd._eval_metric = types.MethodType(tmp_eval_metric, sd)
+    sd.run_init_design()
+    next_point = sd.get_next_point()
+    
+    next_point_expected = np.array([3.9602716910300234e-01, 4.3469440375712098e-02, 9.3294684823072194e-01])
+    inputs_expected = np.array([[0.9660431763890672, 0.2080126306969736, 0.2576380063570568],
+                                [0.0684779421445063, 0.9308367720360009, 0.1428493015158686],
+                                [0.6345029195085983, 0.6651343562344474, 0.8827198350687029],
+                                [0.4531112960399023, 0.3977273628763245, 0.5867585643640021],
+                                [3.9602716910300234e-01, 4.3469440375712098e-02, 9.3294684823072194e-01]])
+    candidates_expected = np.array([[3.9602716910300234e-01, 4.3469440375712098e-02, 9.3294684823072194e-01],
+                                    [6.7353171430888004e-01, 3.2967090024809237e-01, 4.7561098969781079e-01],
+                                    [9.9391552790043791e-01, 5.0015522522360367e-01, 2.4850731127782361e-01],
+                                    [6.6098271480205528e-04, 9.7616245971529891e-01, 7.4815518971467765e-01]])
+    assert_allclose(next_point, next_point_expected)
+    assert_allclose(sd.inputs, inputs_expected)
+    assert_allclose(sd.candidates, candidates_expected)
+    
+def test_SequentialDesign_set_next_target():
+    "test the set_next_target method"
+    
+    np.random.seed(74632)
+    
+    ed = LatinHypercubeDesign(3)
+    
+    def f(x):
+        return np.sum(x)
+        
+    def tmp_eval_metric(self):
+        return 0
+        
+    sd = SequentialDesign(ed, f, n_init = 4, n_cand = 4)
+    
+    sd._eval_metric = types.MethodType(tmp_eval_metric, sd)
+    sd.run_init_design()
+    next_point = sd.get_next_point()
+    new_target = np.reshape(np.array(np.sum(next_point)), (1,))
+    
+    inputs_expected = np.array([[0.9660431763890672, 0.2080126306969736, 0.2576380063570568],
+                                [0.0684779421445063, 0.9308367720360009, 0.1428493015158686],
+                                [0.6345029195085983, 0.6651343562344474, 0.8827198350687029],
+                                [0.4531112960399023, 0.3977273628763245, 0.5867585643640021],
+                                [3.9602716910300234e-01, 4.3469440375712098e-02, 9.3294684823072194e-01]])
+    targets_expected = np.array([[np.sum(i) for i in inputs_expected]])
+    
+    sd.set_next_target(new_target)
+    assert_allclose(sd.targets, targets_expected)
+    assert sd.current_iteration == 5
+    
+        
+    sd = SequentialDesign(ed, f, n_init = 4, n_cand = 4)
+    
+    sd._eval_metric = types.MethodType(tmp_eval_metric, sd)
+    sd.run_init_design()
+    next_point = sd.get_next_point()
+    
+    with pytest.raises(AssertionError):
+        sd.set_next_target(np.zeros((2,)))
+        
+        
+    sd = SequentialDesign(ed, f, n_init = 4, n_cand = 4)
+    
+    sd.generate_initial_design()
+    sd._eval_metric = types.MethodType(tmp_eval_metric, sd)
+    next_point = sd.get_next_point()
+    next_target = np.sum(next_point)
+    
+    with pytest.raises(ValueError):
+        sd.set_next_target(next_target)
+        
+    sd = SequentialDesign(ed, f, n_init = 4, n_cand = 4)
+    
+    with pytest.raises(ValueError):
+        sd.set_next_target(next_target)
+        
+    sd = SequentialDesign(ed, f, n_init = 4, n_cand = 4)
+    sd._eval_metric = types.MethodType(tmp_eval_metric, sd)
+    sd.run_init_design()
+    
+    with pytest.raises(AssertionError):
+        sd.set_next_target(next_target)
+        
+    sd = SequentialDesign(ed, f, n_init = 4, n_cand = 4)
+    sd._eval_metric = types.MethodType(tmp_eval_metric, sd)
+    sd.run_init_design()
+    sd.get_next_point()
+    sd.targets = np.zeros((5,3))
+    
+    with pytest.raises(AssertionError):
+        sd.set_next_target(next_target)
