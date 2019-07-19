f6a8be2401c906bf448b04dd542fc376696f1713
Eric Daub <edaub@turing.ac.uk>
2019-07-19 14:04:08 +0100

    added function to compute gradient of the squared exponential kernel

--- mogp_emulator/Kernel.py
+++ mogp_emulator/Kernel.py
@@ -35,9 +35,9 @@ def calc_r(x1, x2, params):
     
     exp_theta = np.exp(params)
     
-    Q = cdist(np.sqrt(exp_theta[:(D - 1)])*x1, np.sqrt(exp_theta[:(D-1)])*x2, "euclidean")
+    r_matrix = cdist(np.sqrt(exp_theta[:(D - 1)])*x1, np.sqrt(exp_theta[:(D-1)])*x2, "euclidean")
     
-    return Q
+    return r_matrix
     
 def squared_exponential(x1, x2, params):
     "compute kernel values for squared exponential"
@@ -47,11 +47,57 @@ def squared_exponential(x1, x2, params):
     D = len(params)
     assert D >= 2, "minimum number of parameters in a covariance kernel is 2"
     
-    Q = calc_r(x1, x2, params)
-    Q = np.exp(params[D - 1]) * np.exp(-0.5 * Q**2)
+    K = calc_r(x1, x2, params)
+    K = np.exp(params[D - 1]) * np.exp(-0.5 * K**2)
     
-    return Q
+    return K
+
+def squared_exponential_deriv(x1, x2, params):
+    "compute gradient of squared_exponential kernel"
     
+    params = np.array(params)
+    assert params.ndim == 1, "parameters must be a vector"
+    D = len(params)
+    assert D >= 2, "minimum number of parameters in a covariance kernel is 2"
+    
+    x1 = np.array(x1)
+    
+    assert x1.ndim == 1 or x1.ndim == 2, "bad number of dimensions in input x1"
+    
+    if x1.ndim == 2:
+        assert x1.shape[1] == D - 1, "bad shape for x1"
+    else:
+        if D == 2:
+            x1 = np.reshape(x1, (len(x1), 1))
+        else:
+            x1 = np.reshape(x1, (1, D - 1))
+    
+    n1 = x1.shape[0]
+            
+    x2 = np.array(x2)
+    
+    assert x2.ndim == 1 or x2.ndim == 2, "bad number of dimensions in input x2"
+    
+    if x2.ndim == 2:
+        assert x2.shape[1] == D - 1, "bad shape for x2"
+    else:
+        if D == 2:
+            x2 = np.reshape(x2, (len(x2), 1))
+        else:
+            x2 = np.reshape(x2, (1, D - 1))
+            
+    n2 = x2.shape[0]
+    
+    dKdtheta = np.zeros((D, n1, n2))
+    
+    dKdtheta[-1] = squared_exponential(x1, x2, params)
+    
+    for d in range(D - 1):
+        dKdtheta[d] = - 0.5 * np.exp(params[d]) * cdist(np.reshape(x1[:,d], (n1, 1)),
+                                                        np.reshape(x2[:,d], (n2, 1)), "sqeuclidean") * dKdtheta[-1]
+    
+    return dKdtheta
+
 def matern_5_2(x1, x2, params):
     "compute covariance using the matern 5/2 kernel"
     
--- mogp_emulator/tests/test_Kernel.py
+++ mogp_emulator/tests/test_Kernel.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 from numpy.testing import assert_allclose
-from ..Kernel import calc_r, squared_exponential, matern_5_2
+from ..Kernel import calc_r, squared_exponential, squared_exponential_deriv, matern_5_2
 
 def test_calc_r():
     "test function for calc_r function for kernels"
@@ -117,7 +117,100 @@ def test_squared_exponential_failures():
     
     with pytest.raises(AssertionError):
         squared_exponential(x, y, params)
+
+
+def test_squared_exponential_deriv():
+    "test the computation of the gradient of the squared exponential kernel"
+
+    x = np.array([[1.], [2.]])
+    y = np.array([[2.], [3.]])
+    params = np.array([0., 0.])
     
+    deriv = np.zeros((2, 2, 2))
+    
+    deriv[-1] = np.exp(-0.5*np.array([[1., 2.], [0., 1.]])**2)
+    deriv[0] = -0.5*np.array([[1., 4.],[0., 1.]])*np.exp(-0.5*np.array([[1., 2.], [0., 1.]])**2)
+    
+    assert_allclose(squared_exponential_deriv(x, y, params), deriv)
+    
+    x = np.array([[1., 2.], [2., 3.]])
+    y = np.array([[2., 4.], [3., 1.]])
+    params = np.array([0., 0., 0.])
+    
+    deriv = np.zeros((3, 2, 2))
+    
+    deriv[-1] = np.exp(-0.5*np.array([[np.sqrt(5.), np.sqrt(5.)], [1., np.sqrt(5.)]])**2)
+    deriv[0] = -0.5*np.array([[1., 4.],[0., 1.]])*deriv[-1]
+    deriv[1] = -0.5*np.array([[4., 1.],[1., 4.]])*deriv[-1]
+    
+    assert_allclose(squared_exponential_deriv(x, y, params), deriv)
+    
+    x = np.array([[1., 2.], [2., 3.]])
+    y = np.array([[2., 4.], [3., 1.]])
+    params = np.array([np.log(2.), np.log(4.), np.log(2.)])
+    
+    deriv = np.zeros((3, 2, 2))
+    
+    deriv[-1] = 2.*np.exp(-0.5*np.array([[np.sqrt(1.*2.+4.*4.), np.sqrt(4.*2.+1.*4.)], [np.sqrt(1.*4.), np.sqrt(1.*2.+4.*4.)]])**2)
+    deriv[0] = -0.5*np.array([[2., 8.],[0., 2.]])*deriv[-1]
+    deriv[1] = -0.5*np.array([[16., 4.],[4., 16.]])*deriv[-1]
+    
+    assert_allclose(squared_exponential_deriv(x, y, params), deriv)
+                    
+    x = np.array([1., 2.])
+    y = np.array([2., 3.])
+    params = np.array([0., 0.])
+    
+    deriv = np.zeros((2, 2, 2))
+    
+    deriv[-1] = np.exp(-0.5*np.array([[1., 2.], [0., 1.]])**2)
+    deriv[0] = -0.5*np.array([[1., 4.],[0., 1.]])*np.exp(-0.5*np.array([[1., 2.], [0., 1.]])**2)
+    
+    assert_allclose(squared_exponential_deriv(x, y, params), deriv)
+
+def test_squared_exponential_deriv_failures():
+    "test scenarios where squared_exponential should raise an exception"
+    
+    x = np.array([[1.], [2.]])
+    y = np.array([[2.], [3.]])
+    params = np.array([0.])
+    
+    with pytest.raises(AssertionError):
+        squared_exponential_deriv(x, y, params)
+    
+    params = np.array([[0., 0.], [0., 0.]])
+    
+    with pytest.raises(AssertionError):
+        squared_exponential_deriv(x, y, params)
+        
+    x = np.array([[1.], [2.]])
+    y = np.array([[2., 4.], [3., 2.]])
+    params = np.array([0., 0.])
+    
+    with pytest.raises(AssertionError):
+        squared_exponential_deriv(x, y, params)
+        
+    x = np.array([[1.], [2.]])
+    y = np.array([[[2.], [4.]], [[3.], [2.]]])
+    params = np.array([0., 0.])
+    
+    with pytest.raises(AssertionError):
+        squared_exponential_deriv(x, y, params)
+        
+    x = np.array([[2., 4.], [3., 2.]])
+    y = np.array([[1.], [2.]])
+    params = np.array([0., 0.])
+    
+    with pytest.raises(AssertionError):
+        squared_exponential_deriv(x, y, params)
+        
+    x = np.array([[[2.], [4.]], [[3.], [2.]]])
+    y = np.array([[1.], [2.]])
+    params = np.array([0., 0.])
+    
+    with pytest.raises(AssertionError):
+        squared_exponential_deriv(x, y, params)
+
 def test_matern_5_2():
     "test matern 5/2 covariance kernel"
     
