8b1aa86ba5abb09291b3717f05cab925deeb1b36
Eric Daub <edaub@turing.ac.uk>
2019-08-02 16:09:25 +0100

    fully implemented basic MCMC sampler

--- mogp_emulator/MCMC.py
+++ mogp_emulator/MCMC.py
@@ -34,6 +34,50 @@ def MCMC_step(loglikelihood, current_param, step_sizes, loglike_sign = 1.):
 
     return next_point, accept
 
+def sample_MCMC(loglikelihood, start, step_size, n_samples = 1000, thin = 0, loglike_sign = 1.):
+    "draw MCMC samples for a loglikelihood given weak priors"
+
+    n_samples = int(n_samples)
+    thin = int(thin)
+
+    assert n_samples > 0, "number of samples must be a positive integer"
+    assert thin >= 0, "thin must be a non-negative integer"
+    
+    start = np.array(start)
+    
+    assert start.ndim == 1, "starting point must be a 1d array"
+    n_params = len(start)
+
+    samples = np.zeros((n_samples, n_params))
+    samples[0] = start
+    rejected = []
+
+    for i in range(n_samples - 1):
+        next_point, accept = MCMC_step(loglikelihood, samples[i], step_size, loglike_sign)
+        if accept:
+            samples[i+1] = np.copy(next_point)
+        else:
+            samples[i+1] = np.copy(samples[i])
+            rejected.append(np.array(next_point))
+        
+    acceptance = float(n_samples - len(rejected))/float(n_samples)
+
+    if thin == 0:
+        thin_freq = autothin_samples(samples)
+    else:
+        thin_freq = thin
+    
+    thinned = samples[::thin_freq]
+
+    first_lag = np.zeros(n_params)
+
+    if n_samples > 1:
+        for i in range(n_params):
+            autocorr = np.correlate(thinned[:,i]-np.mean(thinned[:,i]), thinned[:,i]-np.mean(thinned[:,i]), mode="full")
+            first_lag[i] = autocorr[np.argmax(autocorr)+1]/np.max(autocorr)
+
+    return thinned, np.array(rejected), acceptance, first_lag
+
 def autothin_samples(signal):
     "automatically determine thinning needed to obtain uncorrelated samples"
     
@@ -47,16 +91,17 @@ def autothin_samples(signal):
     
     maxthin = 0
     
-    for i in range(n_params):
-        autocorr = np.correlate(signal[:,i]-np.mean(signal[:,i]), signal[:,i]-np.mean(signal[:,i]), mode="same")
-        start = np.argmax(autocorr)
-        autocorr = autocorr/np.max(autocorr)
-        for j in range(1, n_samples - start):
-            print(i, j, autocorr[start + j])
-            if np.abs(autocorr[start + j]) < 3./np.sqrt(float(n_samples)):
-                if j > maxthin:
-                    maxthin = j
-                break
+    if n_samples >= 10:
+        for i in range(n_params):
+            autocorr = np.correlate(signal[:,i]-np.mean(signal[:,i]), signal[:,i]-np.mean(signal[:,i]), mode="full")
+            start = np.argmax(autocorr)
+            if np.max(autocorr) > 0.:
+                autocorr = autocorr/np.max(autocorr)
+            for j in range(1, len(autocorr) - start):
+                if np.abs(autocorr[start + j]) < 3./np.sqrt(float(n_samples)):
+                    if j > maxthin:
+                        maxthin = j
+                    break
     
     if maxthin == 0:
         print("automatic thinning failed, posterior distribution may be multimodal")
--- mogp_emulator/tests/test_MCMC.py
+++ mogp_emulator/tests/test_MCMC.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 from numpy.testing import assert_allclose
-from ..MCMC import MH_proposal, MCMC_step, autothin_samples
+from ..MCMC import MH_proposal, MCMC_step, sample_MCMC, autothin_samples
 
 def test_MH_proposal():
     "test the Metropolis-Hastings proposal distribution"
@@ -75,6 +75,64 @@ def test_MCMC_step():
     with pytest.raises(AssertionError):
         MCMC_step(2., current_param, cov)
         
+def test_sample_MCMC():
+    "test MCMC sampling"
+    
+    def loglikelihood(x):
+        return 100.*np.sum(x)
+    
+    current_param = np.zeros(2)
+    cov = np.eye(2)
+    
+    samples_expected = np.array([[0., 0.],
+                                [-0.392811129690724 ,  0.4478392215027822],
+                                [-1.6681438506139665,  2.697940030289671 ],
+                                [-1.6681438506139665,  2.697940030289671 ]])
+    rejected_expected = np.array([[-3.4166078360234584,  2.5824123019889043]])
+    acceptance_expected = 0.75
+    first_lag_expected = np.array([0.2886356417500504, 0.2824351401835904])
+    
+    np.random.seed(438)
+    
+    samples, rejected, acceptance, first_lag = sample_MCMC(loglikelihood, current_param, cov, n_samples = 4, thin = 0)
+    
+    assert_allclose(samples, samples_expected)
+    assert_allclose(rejected, rejected_expected)
+    assert_allclose(acceptance, acceptance_expected)
+    assert_allclose(first_lag, first_lag_expected)
+    
+    np.random.seed(438)
+    
+    samples, rejected, acceptance, first_lag = sample_MCMC(loglikelihood, current_param, cov, n_samples = 4, thin = 1)
+    
+    assert_allclose(samples, samples_expected)
+    assert_allclose(rejected, rejected_expected)
+    assert_allclose(acceptance, acceptance_expected)
+    assert_allclose(first_lag, first_lag_expected)
+    
+    samples_expected = np.array([[0., 0.],
+                                [-1.6681438506139665,  2.697940030289671 ]])
+    rejected_expected = np.array([[-3.4166078360234584,  2.5824123019889043]])
+    acceptance_expected = 0.75
+    first_lag_expected = np.array([-0.5, -0.5])
+    
+    np.random.seed(438)
+    
+    samples, rejected, acceptance, first_lag = sample_MCMC(loglikelihood, current_param, cov, n_samples = 4, thin = 2)
+    
+    assert_allclose(samples, samples_expected)
+    assert_allclose(rejected, rejected_expected)
+    assert_allclose(acceptance, acceptance_expected)
+    assert_allclose(first_lag, first_lag_expected)
+    
+    with pytest.raises(AssertionError):
+        sample_MCMC(loglikelihood, current_param, cov, n_samples = -1)
+        
+    with pytest.raises(AssertionError):
+        sample_MCMC(loglikelihood, current_param, cov, 1000, -1)
+        
+    with pytest.raises(AssertionError):
+        sample_MCMC(loglikelihood, np.zeros((2,3)), cov, 1000, -1)
 
 def test_autothin_samples():
     "test the autothinning routine"
@@ -89,6 +147,10 @@ def test_autothin_samples():
     a[40:60, 1] = 1.
     
     assert autothin_samples(a) == 11
+    
+    a = np.zeros(9)
+    
+    assert autothin_samples(a) == 1
     
     a = np.zeros((2, 2, 2))
     
