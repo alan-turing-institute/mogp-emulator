22f386905c94fdd8c89b74cded6e3be1854ebd15
Eric Daub <edaub@turing.ac.uk>
2019-09-19 17:22:19 +0100

    modifications to conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(1, os.path.abspath('../mogp_emulator'))
+sys.path.insert(0, os.path.abspath('../mogp_emulator'))
 
 
 # -- Project information -----------------------------------------------------
