6588d66a689af857253e72fa883cd06b46ac9d4c
Jim Madge <jmmadge@gmail.com>
2019-09-27 16:32:52 +0100

    Export KDR object

--- mogp_emulator/__init__.py
+++ mogp_emulator/__init__.py
@@ -2,4 +2,4 @@ from .MultiOutputGP import MultiOutputGP
 from .GaussianProcess import GaussianProcess
 from .ExperimentalDesign import ExperimentalDesign, MonteCarloDesign, LatinHypercubeDesign
 from .SequentialDesign import SequentialDesign, MICEDesign
-from .DimensionReduction import gKDR
+from .DimensionReduction import gKDR, KDR
