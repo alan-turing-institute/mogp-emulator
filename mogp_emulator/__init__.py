2d141f9a66c41a915c24efa4e2202d0e8e036ec9
Eric Daub <45598892+edaub@users.noreply.github.com>
2019-10-22 14:00:01 +0100

    Versioning (#38)
    * added code needed for versioning to devel branch
    
    * forgot to modify setup.py file
    
    * corrected line accidentally deleted from __init__.py
    
    * added prerelease number to devel branch to track commits on devel
    
    * corrected comments in conf.py to reflect full release numbering

--- mogp_emulator/__init__.py
+++ mogp_emulator/__init__.py
@@ -1,3 +1,5 @@
+from .version import version as __version__
+
 from .MultiOutputGP import MultiOutputGP
 from .GaussianProcess import GaussianProcess
 from .ExperimentalDesign import ExperimentalDesign, MonteCarloDesign, LatinHypercubeDesign
--- setup.py
+++ setup.py
@@ -1,15 +1,46 @@
 import setuptools
 
+# version information
+MAJOR = 0
+MINOR = 2
+MICRO = 0
+PRERELEASE = 0
+ISRELEASED = False
+version = "{}.{}.{}".format(MAJOR, MINOR, MICRO)
+
+if not ISRELEASED:
+    version += ".dev{}".format(PRERELEASE)
+
+# write version information to file
+
+def write_version_file(version):
+    "writes version file that is read when importing version number"
+    version_file = """'''
+Version file automatically created by setup.py file
+'''
+version = '{}'
+    """.format(version)
+
+    with open("mogp_emulator/version.py", "w") as fh:
+        fh.write(version_file)
+
+write_version_file(version)
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='mogp_emulator',
-      version='0.1',
+      version=version,
       description='Tool for Multi-Output Gaussian Process Emulators',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='TBD',
-      author='Eric Daub',
+      url='https://mogp-emulator.readthedocs.io/',
+      project_urls={
+                  "Bug Tracker": "https://github.com/alan-turing-institute/mogp_emulator/issues",
+                  "Documentation": "https://mogp-emulator.readthedocs.io/",
+                  "Source Code": "https://github.com/alan-turing-institute/mogp_emulator/",
+              },
+      author='Alan Turing Institute Research Engineering Group',
       author_email='edaub@turing.ac.uk',
       packages=setuptools.find_packages(),
       license=['MIT'],
