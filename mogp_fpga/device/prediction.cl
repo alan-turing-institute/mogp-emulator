32891659bc32d7a0e7fc961d29e3bf5867ee2746
Jim Madge <jmmadge@gmail.com>
2019-12-11 11:39:21 +0000

    Add variance calculation option
    Contributes to #67

--- mogp_fpga/device/prediction.cl
+++ mogp_fpga/device/prediction.cl
@@ -2,6 +2,9 @@
 #define MAX_M 128
 #define MAX_N 128
 
+#define MODE_EXPECTATION 1
+#define MODE_VARIANCE 2
+
 // Determine K(X,Y) for the squared exponential Kernel
 //
 // x is an (nx,dim) array, y is an (ny,dim) array both representing n vectors
@@ -13,14 +16,18 @@
 // sigma is a prefactor, each element of K is multiplied by exp(sigma)
 //
 // The output is a (nx,ny) array which is written one column at a time to the
-// pipe k
+// pipe k1 or k2 depending on the mode
 //
 // nx, ny are the number of vectors in x and y respectively
 // dim is the length of each of the vectors in x and y
+//
+// mode sets whether to calculate only the expectation values (mode=1) or
+// expectation values and variance (mode=2)
 kernel void sq_exp(global float* restrict x, global float* restrict y,
-                   write_only pipe float __attribute__((blocking)) k,
+                   write_only pipe float __attribute__((blocking)) k1,
+                   write_only pipe float __attribute__((blocking)) k2,
                    global float* restrict l, float sigma, int nx, int ny,
-                   int dim){
+                   int dim, int mode){
     // Cache the scaling factors
     float l_cache[MAX_DIM];
     for(unsigned i=0; i<MAX_DIM; i++){
@@ -66,38 +73,106 @@ kernel void sq_exp(global float* restrict x, global float* restrict y,
             // Calculate the element k[row,col] of the square exponentia kernel
             elem = exp_sigma*exp(-0.5f * elem);
             // Push the element to the pipe
-            write_pipe(k, &elem);
+            if (mode == MODE_EXPECTATION){
+                write_pipe(k1, &elem);
+            } else if (mode == MODE_VARIANCE){
+                write_pipe(k2, &elem);
+            }
         }
     }
 }
 
-// Determine the product c=K^Tb where K is a (m,n) matrix, and b and c are (m)
-// vectors (K^T means the transpose of the matrix K).
-kernel void matrix_vector_product(
-    read_only pipe float __attribute__((blocking)) k,
-    global float* restrict b, global float* restrict c, int m, int n
+// Determine the expectation values of predictions
+kernel void prediction(
+    read_only pipe float __attribute__((blocking)) k1,
+    global float* restrict invqt,
+    global float* restrict expectation,
+    int m, int n
     ){
-    // Copy b to local memory
-    float b_cache[MAX_M];
+    // Copy invqt to local memory
+    float invqt_cache[MAX_M];
     for (unsigned i=0; i<m; i++){
-        b_cache[i] = b[i];
+        invqt_cache[i] = invqt[i];
     }
 
     // Calculate one element of c at at time by finding the 'dot product' of
-    // one column of k with b
+    // one column of k with invqt
     for (unsigned col=0; col<n; col++){
         float sum = 0;
 
         // Cache column of k
         float k_cache[MAX_M];
         for (unsigned i=0; i<m; i++){
-            read_pipe(k, &k_cache[i]);
+            read_pipe(k1, &k_cache[i]);
         }
 
         #pragma unroll
         for (unsigned i=0; i<MAX_M; i++){
-            sum += k_cache[i] * b_cache[i];
+            sum += k_cache[i] * invqt_cache[i];
         }
-        c[col] = sum;
+        expectation[col] = sum;
+    }
+}
+
+// Determine the expectation values and variance of predictions
+kernel void prediction_with_variance(
+    read_only pipe float __attribute__((blocking)) k2,
+    global float* restrict expectation,
+    global float* restrict variance,
+    global float* restrict invqt,
+    global float* restrict invq, float sigma, int m, int n
+    ){
+    // Copy invq and invqt to local memory
+    local float invqt_cache[MAX_M];
+    for (unsigned i=0; i<m; i++){
+        invqt_cache[i] = invqt[i];
+    }
+    local float invq_cache[MAX_M*MAX_M];
+    for (unsigned i=0; i<m; i++){
+        int offset1 = i*m;
+        int offset2 = i*MAX_M;
+        for (unsigned j=0; j<m; j++){
+            invq_cache[offset2+j] = invq[offset1+j];
+        }
+    }
+    // Take exponential of sigma
+    local float exp_sigma;
+    exp_sigma = exp(sigma);
+
+    for (unsigned col=0; col<n; col++){
+        float sum = 0;
+
+        // Cache column of k
+        float k_cache[MAX_M];
+        for (unsigned i=0; i<m; i++){
+            read_pipe(k2, &k_cache[i]);
+        }
+
+        // Expectation value calculation
+        // dot product of one column of k with invqt
+        #pragma unroll
+        for (unsigned i=0; i<MAX_M; i++){
+            sum += k_cache[i] * invqt_cache[i];
+        }
+        expectation[col] = sum;
+
+
+        // Variance calculation
+        // dot products of the column of k with each row of invQ
+        // multiplied elementwise by the column of k
+        // sum over
+        float var = 0;
+        for (unsigned row=0; row<m; row++){
+            int offset = row*MAX_M;
+            float dot_product = 0;
+            #pragma unroll
+            for (unsigned i=0; i<MAX_M; i++){
+                dot_product += k_cache[i] * invq_cache[offset+i];
+            }
+            var += dot_product * k_cache[row];
+        }
+        // Subtract from hyperparameter
+        var = exp_sigma - var;
+        variance[col] = max(var, 0.0f);
     }
 }
--- mogp_fpga/host/prediction.cpp
+++ mogp_fpga/host/prediction.cpp
@@ -11,44 +11,8 @@
 #define MAX_M 128
 #define MAX_N 128
 
-void square_exponential_native(std::vector<float> r, std::vector<float> &k,
-                               float sigma){
-    std::transform(r.begin(), r.end(), k.begin(),
-                   [](float x) -> float { return exp(-0.5f * x); });
-    float exp_sigma = exp(sigma);
-    for (auto i=k.begin(); i!=k.end(); i++){
-        *i *= exp_sigma;
-    }
-}
-
-void distance_native(std::vector<float> x, std::vector<float> y,
-                     std::vector<float> &r, std::vector<float> l,
-                     int nx, int ny, int dim){
-    for (int row=0; row<nx; row++){
-        int row_stride = row*dim;
-        for (int col=0; col<ny; col++){
-            int col_stride = col*dim;
-
-            float sum = 0;
-            for (int i=0; i<dim; i++){
-                float difference = x[row_stride+i] - y[col_stride+i];
-                sum += (difference * difference) / exp(-1.0f*l[i]);
-            }
-            r[row*ny+col] = sum;
-        }
-    }
-}
-
-void matrix_vector_product_native(std::vector<float> a, std::vector<float> b,
-                                  std::vector<float> &c, int m, int n){
-    for (int col=0; col<n; col++){
-        float sum = 0.0f;
-        for (int row=0; row<m; row++){
-            sum += a[row*n+col] * b[row];
-        }
-        c[col] = sum;
-    }
-}
+#define MODE_EXPECTATION 1
+#define MODE_VARIANCE 2
 
 void compare_results(std::vector<float> expected, std::vector<float> actual,
                      std::string kernel_name){
@@ -77,49 +41,117 @@ void compare_results(std::vector<float> expected, std::vector<float> actual,
 // nx_star - Number of testing inputs
 // theta - Hyperparameters (used to scale pairwise distances)
 // sigma - Kernel scaling parameter
-// InvQt - Vector which K(X,X*) is multiplied by to obtain expectation values
-std::vector<float> predict_single(std::vector<float> &X, int nx, int dim,
-                                  std::vector<float> &Xstar, int nxstar,
-                                  std::vector<float> &theta, float sigma,
-                                  std::vector<float> &InvQt,
-                                  cl::Context &context,
-                                  cl::Program &program){
-    // Prediction results array
-    std::vector<float> Ystar(nxstar, 0);
+// InvQt - Vector calculated during training (invQ*Y) used for expecation value
+//         calculation
+// Ystar - Training prediction expectation values
+// context - The OpenCL Context
+// program - The OpenCL Program
+void predict_single(std::vector<float> &X, int nx, int dim,
+                    std::vector<float> &Xstar, int nxstar,
+                    std::vector<float> &theta, float sigma,
+                    std::vector<float> &InvQt,
+                    std::vector<float> &Ystar,
+                    cl::Context &context, cl::Program &program){
 
     // Create queues
     cl::CommandQueue queue1(context);
     cl::CommandQueue queue2(context);
 
     // Create kernel functor for square exponential kernel
-    auto square_exponential = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::Pipe&, cl::Buffer, float, int, int, int>(program, "sq_exp");
+    auto square_exponential = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::Pipe&, cl::Pipe&, cl::Buffer, float, int, int, int, int>(program, "sq_exp");
     // Create kernel functor for matrix vector product kernel
-    auto matrix_vector_product = cl::KernelFunctor<cl::Pipe&, cl::Buffer, cl::Buffer, int, int>(program, "matrix_vector_product");
+    auto predict = cl::KernelFunctor<cl::Pipe&, cl::Buffer, cl::Buffer, int, int>(program, "prediction");
 
     // Create device variables
     cl::Buffer d_X(X.begin(), X.end(), true);
     cl::Buffer d_Xstar(Xstar.begin(), Xstar.end(), true);
     cl::Buffer d_theta(theta.begin(), theta.end(), true);
     cl::Buffer d_InvQt(InvQt.begin(), InvQt.end(), true);
-    //cl::Pipe pipe(context, sizeof(cl_float), MAX_M);
     cl_int status;
-    cl_mem pipe_k = clCreatePipe(context(), 0, sizeof(cl_float), MAX_N, NULL, &status);
+    cl_mem pipe_k = clCreatePipe(context(), 0, sizeof(cl_float), MAX_M, NULL, &status);
     cl::Pipe k(pipe_k);
+    cl_mem pipe_dummy = clCreatePipe(context(), 0, sizeof(int), 0, NULL, &status);
+    cl::Pipe dummy(pipe_dummy);
     cl::Buffer d_Ystar(Ystar.begin(), Ystar.end(), false);
 
     // Prediction
     // Determine square exponential kernel matrix
     square_exponential(cl::EnqueueArgs(queue1, cl::NDRange(1)), d_X,
-                       d_Xstar, k, d_theta, sigma, nx, nxstar, dim);
+                       d_Xstar, k, dummy, d_theta, sigma, nx, nxstar, dim,
+                       MODE_EXPECTATION);
     // Columns of the kernel matrix are sent by a pipe to the matrix vector
     // product matrix
-    matrix_vector_product(cl::EnqueueArgs(queue2, cl::NDRange(1)), k,
-                          d_InvQt, d_Ystar, nx, nxstar);
+    predict(cl::EnqueueArgs(queue2, cl::NDRange(1)), k, d_InvQt, d_Ystar, nx,
+            nxstar);
+    queue1.finish();
     queue2.finish();
 
     // Retreive expectation values
     cl::copy(d_Ystar, Ystar.begin(), Ystar.end());
-    return Ystar;
+}
+
+// Conduct a single prediction using the square exponetial kernel
+// X - Training inputs
+// nx - Number of training inputs
+// dim - The number of input dimensions (length of X and Xstar records)
+// Xstar - Testing inputs
+// nx_star - Number of testing inputs
+// theta - Hyperparameters (used to scale pairwise distances)
+// sigma - Kernel scaling parameter
+// InvQt - Vector calculated during training (invQ*Y) used for expecation value
+//         calculation
+// invQ - Matrix calculated during training ((K(X,X) + nugget)^-1) used for
+//        variance calculation
+// Ystar - Training prediction expectation values
+// Ystarvar - Training prediction variances
+// context - The OpenCL Context
+// program - The OpenCL Program
+void predict_single(std::vector<float> &X, int nx, int dim,
+                    std::vector<float> &Xstar, int nxstar,
+                    std::vector<float> &theta, float sigma,
+                    std::vector<float> &InvQt, std::vector<float> &InvQ,
+                    std::vector<float> &Ystar, std::vector<float> &Ystarvar,
+                    cl::Context &context, cl::Program &program){
+
+    // Create queues
+    cl::CommandQueue queue1(context);
+    cl::CommandQueue queue2(context);
+
+    // Create kernel functor for square exponential kernel
+    auto square_exponential = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::Pipe&, cl::Pipe&, cl::Buffer, float, int, int, int, int>(program, "sq_exp");
+    // Create kernel functor for matrix vector product kernel
+    auto predict = cl::KernelFunctor<cl::Pipe&, cl::Buffer, cl::Buffer, cl::Buffer, cl::Buffer, float, int, int>(program, "prediction_with_variance");
+
+    // Create device variables
+    cl::Buffer d_X(X.begin(), X.end(), true);
+    cl::Buffer d_Xstar(Xstar.begin(), Xstar.end(), true);
+    cl::Buffer d_theta(theta.begin(), theta.end(), true);
+    cl::Buffer d_InvQt(InvQt.begin(), InvQt.end(), true);
+    cl::Buffer d_InvQ(InvQ.begin(), InvQ.end(), true);
+    //cl::Pipe pipe(context, sizeof(cl_float), MAX_M);
+    cl_int status;
+    cl_mem pipe_k = clCreatePipe(context(), 0, sizeof(cl_float), MAX_M, NULL, &status);
+    cl::Pipe k(pipe_k);
+    cl_mem pipe_dummy = clCreatePipe(context(), 0, sizeof(int), 0, NULL, &status);
+    cl::Pipe dummy(pipe_dummy);
+    cl::Buffer d_Ystar(Ystar.begin(), Ystar.end(), false);
+    cl::Buffer d_Ystarvar(Ystarvar.begin(), Ystarvar.end(), false);
+
+    // Prediction
+    // Determine square exponential kernel matrix
+    square_exponential(cl::EnqueueArgs(queue1, cl::NDRange(1)), d_X,
+                       d_Xstar, dummy, k, d_theta, sigma, nx, nxstar, dim,
+                       MODE_VARIANCE);
+    // Columns of the kernel matrix are sent by a pipe to the matrix vector
+    // product matrix
+    predict(cl::EnqueueArgs(queue2, cl::NDRange(1)), k, d_Ystar, d_Ystarvar,
+            d_InvQt, d_InvQ, sigma, nx, nxstar);
+    queue1.finish();
+    queue2.finish();
+
+    // Retreive expectation values
+    cl::copy(d_Ystar, Ystar.begin(), Ystar.end());
+    cl::copy(d_Ystarvar, Ystarvar.begin(), Ystarvar.end());
 }
 
 int main(){
@@ -176,32 +208,40 @@ int main(){
         int nx = 3; int nxstar = 2;
         // Dimension of inputs
         int dim = 3;
-        // Square distances between X and X*
-        std::vector<float> r_native(nx*nxstar, 0);
         // InvQt vector, a product of training
         std::vector<float> h_InvQt = {1.9407565, 2.93451157, 3.95432381};
+        // InvQ matrix, a product of training
+        std::vector<float> h_InvQ = { 1.0001672 , -0.01103735, -0.00661646,
+                                     -0.01103735,  1.00024523, -0.01103735,
+                                     -0.00661646, -0.01103735,  1.0001672};
         // Hyperparameter used to scale predictions
         float sigma = 0.0f;
         // Hyperparameters to set length scale of distances between inputs
         std::vector<float> h_l = {0.0, 0.0, 0.0};
-        // Kernel matrix
-        std::vector<float> k_native(nx*nxstar, 0);
         // Prediction result
-        std::vector<float> h_Ystar_native(nxstar, 0);
+        std::vector<float> h_Ystar(nxstar, 0);
+        // Prediction variance
+        std::vector<float> h_Ystarvar(nxstar, 0);
 
         // Expected results
         std::vector<float> expected_Ystar = {1.39538648, 1.73114001};
+        std::vector<float> expected_Ystarvar = {0.81667540, 0.858355920};
 
-        distance_native(h_X, h_Xstar, r_native, h_l, nx, nxstar, dim);
-        square_exponential_native(r_native, k_native, sigma);
-        matrix_vector_product_native(k_native, h_InvQt, h_Ystar_native, nx, nxstar);
-
-        auto h_Ystar = predict_single(h_X, nx, dim, h_Xstar, nxstar, h_l,
-                                      sigma, h_InvQt, context, program);
-
-        compare_results(expected_Ystar, h_Ystar, "matrix_vector_product");
-        compare_results(expected_Ystar, h_Ystar_native, "matrix_vector_product_native");
+        predict_single(h_X, nx, dim, h_Xstar, nxstar, h_l, sigma, h_InvQt,
+                       h_Ystar, context, program);
+        compare_results(expected_Ystar, h_Ystar, "predict expectation values");
         for (auto const& i : h_Ystar)
+            std::cout << i << ' ';
+        std::cout << std::endl;
+
+        predict_single(h_X, nx, dim, h_Xstar, nxstar, h_l, sigma, h_InvQt,
+                       h_InvQ, h_Ystar, h_Ystarvar, context, program);
+        compare_results(expected_Ystar, h_Ystar, "predict expectation values");
+        for (auto const& i : h_Ystar)
+            std::cout << i << ' ';
+        std::cout << std::endl;
+        compare_results(expected_Ystarvar, h_Ystarvar, "predict variance");
+        for (auto const& i : h_Ystarvar)
             std::cout << i << ' ';
         std::cout << std::endl;
 
@@ -214,16 +254,35 @@ int main(){
         //
         // X* = [4,0,2]
         // Expected Y* = [0.01741762]
-        h_Xstar = {4.0, 0.0, 2.0};
         nxstar = 1;
+        h_Xstar.resize(nxstar*dim);
+        h_Xstar = {4.0, 0.0, 2.0};
+        h_Ystar.resize(nxstar);
+        h_Ystarvar.resize(nxstar);
         h_InvQt = {0.73575167, 1.10362757, 1.47151147};
+        h_InvQ = { 3.67879441e-01, -1.79183651e-06, -4.60281258e-07,
+                  -1.79183651e-06,  3.67879441e-01, -1.79183651e-06,
+                  -4.60281258e-07, -1.79183651e-06,  3.67879441e-01};
         h_l = {1.0, 1.0, 1.0};
         sigma = 1.0;
         expected_Ystar = {0.01741762};
-        h_Ystar = predict_single(h_X, nx, dim, h_Xstar, nxstar, h_l,
-                                 sigma, h_InvQt, context, program);
-        compare_results(expected_Ystar, h_Ystar, "matrix_vector_product");
+        expected_Ystarvar = {2.718230287};
+
+        predict_single(h_X, nx, dim, h_Xstar, nxstar, h_l, sigma, h_InvQt,
+                       h_Ystar, context, program);
+        compare_results(expected_Ystar, h_Ystar, "predict expectation values");
         for (auto const& i : h_Ystar)
+            std::cout << i << ' ';
+        std::cout << std::endl;
+
+        predict_single(h_X, nx, dim, h_Xstar, nxstar, h_l, sigma, h_InvQt,
+                       h_InvQ, h_Ystar, h_Ystarvar, context, program);
+        compare_results(expected_Ystar, h_Ystar, "predict expectation values");
+        for (auto const& i : h_Ystar)
+            std::cout << i << ' ';
+        std::cout << std::endl;
+        compare_results(expected_Ystarvar, h_Ystarvar, "predict variance");
+        for (auto const& i : h_Ystarvar)
             std::cout << i << ' ';
         std::cout << std::endl;
     }
