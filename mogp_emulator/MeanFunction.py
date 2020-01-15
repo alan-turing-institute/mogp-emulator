8b0d38935716fc104a9fa8e4c2082d3038539774
edaub <edaub@turing.ac.uk>
2020-01-15 16:54:28 +0000

    initial working implementation of mean function with tests

---
+++ mogp_emulator/MeanFunction.py
@@ -0,0 +1,312 @@
+import numpy as np
+from functools import partial
+
+class MeanFunction(object):
+    def _check_inputs(self, x, params):
+        "check the shape of the inputs"
+
+        if len(x.shape) == 1:
+            x = np.reshape(x, (-1, 1))
+        assert len(x.shape) == 2, "inputs must be a 1D or 2D array"
+
+        assert len(params.shape) == 1, "params must be a 1D array"
+
+        assert len(params) == self.get_n_params(x), "bad length for params"
+
+        return x, params
+
+    def get_n_params(self, x):
+        "determine the number of parameters, possibly which is dependent on x"
+
+        raise NotImplementedError("base mean function does not implement a particular function")
+
+    def mean_f(self, x, params):
+        "returns value of mean function"
+
+        raise NotImplementedError("base mean function does not implement a particular function")
+
+    def mean_deriv(self, x, params):
+        "returns derivative of mean function wrt params"
+
+        raise NotImplementedError("base mean function does not implement a particular function")
+
+    def mean_hessian(self, x, params):
+        "returns hessian of mean function wrt params"
+
+        raise NotImplementedError("base mean function does not implement a particular function")
+
+    def mean_inputderiv(self, x, params):
+        "returns derivative of mean function wrt inputs"
+
+        raise NotImplementedError("base mean function does not implement a particular function")
+
+    def __add__(self, other):
+        "combines two mean functions"
+
+        if issubclass(type(other), MeanFunction):
+            return MeanSum(self, other)
+        elif isinstance(other, (float, int)):
+            return MeanSum(self, FixedMean(other))
+        else:
+            raise TypeError("other function cannot be added with a MeanFunction")
+
+    def __radd__(self, other):
+        "combines two mean functions"
+
+        if issubclass(type(other), MeanFunction):
+            return MeanSum(other, self)
+        elif isinstance(other, (float, int)):
+            return MeanSum(FixedMean(other), self)
+        else:
+            raise TypeError("other function cannot be added with a MeanFunction")
+
+    def __mul__(self, other):
+        "multiplies two mean functions"
+
+        if issubclass(type(other), MeanFunction):
+            return MeanProduct(self, other)
+        elif isinstance(other, (float, int)):
+            return MeanProduct(self, FixedMean(other))
+        else:
+            raise TypeError("other function cannot be added with a MeanFunction")
+
+    def __rmul__(self, other):
+        "multiplies two mean functions"
+
+        if issubclass(type(other), MeanFunction):
+            return MeanProduct(other, self)
+        elif isinstance(other, (float, int)):
+            return MeanProduct(FixedMean(other), self)
+        else:
+            raise TypeError("other function cannot be added with a MeanFunction")
+
+class MeanSum(MeanFunction):
+    def __init__(self, f1, f2):
+
+        if not issubclass(type(f1), MeanFunction):
+            raise TypeError("inputs to MeanSum must be subclasses of MeanFunction")
+        if not issubclass(type(f2), MeanFunction):
+            raise TypeError("inputs to MeanSum must be subclasses of MeanFunction")
+
+        self.f1 = f1
+        self.f2 = f2
+
+    def get_n_params(self, x):
+        return self.f1.get_n_params(x) + self.f2.get_n_params(x)
+
+    def mean_f(self, x, params):
+        "returns value of mean function"
+
+        switch = self.f1.get_n_params(x)
+
+        return (self.f1.mean_f(x, params[:switch]) +
+                self.f2.mean_f(x, params[switch:]))
+
+    def mean_deriv(self, x, params):
+        "returns derivative of mean function wrt params"
+
+        switch = self.f1.get_n_params(x)
+
+        deriv = np.zeros((self.get_n_params(x), x.shape[0]))
+
+        deriv[:switch] = self.f1.mean_deriv(x, params[:switch])
+        deriv[switch:] = self.f2.mean_deriv(x, params[switch:])
+
+        return deriv
+
+    def mean_hessian(self, x, params):
+        "returns hessian of mean function wrt params"
+
+        switch = self.f1.get_n_params(x)
+
+        hess = np.zeros((self.get_n_params(x), self.get_n_params(x), x.shape[0]))
+
+        hess[:switch, :switch] = self.f1.mean_hessian(x, params[:switch])
+        hess[switch:, switch:] = self.f2.mean_hessian(x, params[switch:])
+
+        return hess
+
+    def mean_inputderiv(self, x, params):
+        "returns derivative of mean function wrt inputs"
+
+        switch = self.f1.get_n_params(x)
+
+        return (self.f1.mean_inputderiv(x, params[:switch]) +
+                self.f2.mean_inputderiv(x, params[switch:]))
+
+class MeanProduct(MeanFunction):
+    def __init__(self, f1, f2):
+
+        if not issubclass(type(f1), MeanFunction):
+            raise TypeError("inputs to MeanProduct must be subclasses of MeanFunction")
+        if not issubclass(type(f2), MeanFunction):
+            raise TypeError("inputs to MeanProduct must be subclasses of MeanFunction")
+
+        self.f1 = f1
+        self.f2 = f2
+
+    def get_n_params(self, x):
+        return self.f1.get_n_params(x) + self.f2.get_n_params(x)
+
+    def mean_f(self, x, params):
+        "returns value of mean function"
+
+        switch = self.f1.get_n_params(x)
+
+        return (self.f1.mean_f(x, params[:switch])*
+                self.f2.mean_f(x, params[switch:]))
+
+    def mean_deriv(self, x, params):
+        "returns derivative of mean function wrt params"
+
+        switch = self.f1.get_n_params(x)
+
+        deriv = np.zeros((self.get_n_params(x), x.shape[0]))
+
+        deriv[:switch] = (self.f1.mean_deriv(x, params[:switch])*
+                          self.f2.mean_f(x, params[switch:]))
+
+        deriv[switch:] = (self.f1.mean_f(x, params[:switch])*
+                          self.f2.mean_deriv(x, params[switch:]))
+
+        return deriv
+
+    def mean_hessian(self, x, params):
+        "returns hessian of mean function wrt params"
+
+        switch = self.f1.get_n_params(x)
+
+        hess = np.zeros((self.get_n_params(x), self.get_n_params(x), x.shape[0]))
+
+        hess[:switch, :switch] = (self.f1.mean_hessian(x, params[:switch])*
+                                  self.f2.mean_f(x, params[switch:]))
+        hess[:switch, switch:, :] = (self.f1.mean_deriv(x, params[:switch])[:,np.newaxis,:]*
+                                     self.f2.mean_deriv(x, params[switch:])[np.newaxis,:,:])
+        hess[switch:, :switch, :] = np.transpose(hess[:switch, switch:, :], (1, 0, 2))
+        hess[switch:, switch:] = (self.f1.mean_f(x, params[:switch])*
+                                  self.f2.mean_hessian(x, params[switch:]))
+
+        return hess
+
+
+    def mean_inputderiv(self, x, params):
+        "returns derivative of mean function wrt inputs"
+
+        switch = self.f1.get_n_params(x)
+
+        return (self.f1.mean_inputderiv(x, params[:switch])*
+                self.f2.mean_f(x, params[switch:]) +
+                self.f1.mean_f(x, params[:switch])*
+                self.f2.mean_inputderiv(x, params[switch:]))
+
+class FixedMean(MeanFunction):
+    "a mean function with a fixed function/derivative and no fitting parameters"
+    def __init__(self, *args):
+
+        if len(args) == 1 and isinstance(args[0], (float, int)):
+            val = float(args[0])
+            def f_full(x, val):
+                return np.broadcast_to(val, x.shape[0])
+            f = partial(f_full, val=val)
+            deriv = lambda x: np.zeros((x.shape[1], x.shape[0]))
+        elif len(args) == 1 or len(args) == 2:
+            f = args[0]
+            if len(args) == 1:
+                deriv = None
+            else:
+                deriv = args[1]
+        else:
+            raise ValueError("Bad length of arguments provided to FixedMean")
+
+        assert callable(f), "fixed mean function must be a callable function"
+        if not deriv is None:
+            assert callable(deriv), "mean function derivative must be a callable function"
+
+        self.f = f
+        self.deriv = deriv
+
+    def get_n_params(self, x):
+        return 0
+
+    def mean_f(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return self.f(x)
+
+    def mean_deriv(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.zeros((self.get_n_params(x), x.shape[0]))
+
+    def mean_hessian(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.zeros((self.get_n_params(x), self.get_n_params(x), x.shape[0]))
+
+    def mean_inputderiv(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        if self.deriv is None:
+            raise NotImplementedError("Derivative function was not provided with this FixedMean")
+        else:
+            return self.deriv(x)
+
+class ConstantMean(MeanFunction):
+    def get_n_params(self, x):
+        return 1
+
+    def mean_f(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.broadcast_to([params[0]], x.shape[0])
+
+    def mean_deriv(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.ones((self.get_n_params(x), x.shape[0]))
+
+    def mean_hessian(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.zeros((self.get_n_params(x), self.get_n_params(x), x.shape[0]))
+
+    def mean_inputderiv(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.zeros((x.shape[1], x.shape[0]))
+
+class LinearMean(MeanFunction):
+    def get_n_params(self, x):
+        return x.shape[1]
+
+    def mean_f(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.sum(params*x, axis=1)
+
+    def mean_deriv(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.transpose(x)
+
+    def mean_hessian(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.zeros((self.get_n_params(x), self.get_n_params(x), x.shape[0]))
+
+    def mean_inputderiv(self, x, params):
+
+        x, params = self._check_inputs(x, params)
+
+        return np.broadcast_to(np.reshape(params, (-1, 1)), (x.shape[1], x.shape[0]))
---
+++ mogp_emulator/tests/test_MeanFunction.py
@@ -0,0 +1,181 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+from ..MeanFunction import MeanFunction, MeanSum, MeanProduct, FixedMean, ConstantMean, LinearMean
+
+@pytest.fixture
+def mf():
+    return MeanFunction()
+
+@pytest.fixture
+def x():
+    return np.array([[1., 2., 3.], [4., 5., 6.]])
+
+@pytest.fixture
+def params():
+    return np.array([1., 2., 4.])
+
+def test_MeanFunction(mf, x, params):
+    "test composition of mean functions"
+
+    mf2 = mf + mf
+
+    assert isinstance(mf2, MeanSum)
+
+    mf3 = 5. + mf
+
+    assert isinstance(mf3, MeanSum)
+    assert isinstance(mf3.f1, FixedMean)
+
+    mf4 = mf + 3.
+
+    assert isinstance(mf4, MeanSum)
+    assert isinstance(mf4.f2, FixedMean)
+
+    mf5 = mf*mf
+
+    assert isinstance(mf5, MeanProduct)
+
+    mf6 = 3.*mf
+
+    assert isinstance(mf6, MeanProduct)
+    assert isinstance(mf6.f1, FixedMean)
+
+    mf7 = mf*3.
+
+    assert isinstance(mf7, MeanProduct)
+    assert isinstance(mf7.f2, FixedMean)
+
+def test_MeanFunction_failures(mf, x, params):
+    "test situations of MeanFunction where an exception should be raised"
+
+    with pytest.raises(NotImplementedError):
+        mf.get_n_params(x)
+
+    with pytest.raises(NotImplementedError):
+        mf.mean_f(x, params)
+
+    with pytest.raises(NotImplementedError):
+        mf.mean_deriv(x, params)
+
+    with pytest.raises(NotImplementedError):
+        mf.mean_hessian(x, params)
+
+    with pytest.raises(NotImplementedError):
+        mf.mean_inputderiv(x, params)
+
+    with pytest.raises(TypeError):
+        "3" + mf
+
+    with pytest.raises(TypeError):
+        mf + "3"
+
+    with pytest.raises(TypeError):
+        mf*"3"
+
+    with pytest.raises(TypeError):
+        "3"*mf
+
+def test_FixedMean(x):
+    "test the FixedMean function"
+
+    params = np.zeros(0)
+
+    f = lambda x: np.ones(x.shape[0])
+    deriv = lambda x: np.zeros((x.shape[1], x.shape[0]))
+    fixed_mean = FixedMean(f, deriv)
+
+    assert fixed_mean.get_n_params(x) == 0
+
+    assert_allclose(fixed_mean.mean_f(x, params), np.ones(x.shape[0]))
+    assert_allclose(fixed_mean.mean_f(np.array([1., 2., 3.]), params), np.ones(3))
+    assert_allclose(fixed_mean.mean_deriv(x, params), np.zeros((0, x.shape[0])))
+    assert_allclose(fixed_mean.mean_hessian(x, params), np.zeros((0, 0, x.shape[0])))
+    assert_allclose(fixed_mean.mean_inputderiv(x, params), np.zeros((x.shape[1], x.shape[0])))
+
+def test_ConstantMean(x):
+    "test the constant mean function"
+
+    params = np.ones(1)
+
+    constant_mean = ConstantMean()
+
+    assert constant_mean.get_n_params(x) == 1
+
+    assert_allclose(constant_mean.mean_f(x, params), np.broadcast_to(1., x.shape[0]))
+    assert_allclose(constant_mean.mean_deriv(x, params), np.ones((1, x.shape[0])))
+    assert_allclose(constant_mean.mean_hessian(x, params), np.zeros((1, 1, x.shape[0])))
+    assert_allclose(constant_mean.mean_inputderiv(x, params), np.zeros((x.shape[1], x.shape[0])))
+
+def test_LinearMean(x, params):
+    "test the linear mean function"
+
+    linear_mean = LinearMean()
+
+    assert linear_mean.get_n_params(x) == 3
+
+    assert_allclose(linear_mean.mean_f(x, params), np.sum(x*params, axis=1))
+    assert_allclose(linear_mean.mean_deriv(x, params), np.transpose(x))
+    assert_allclose(linear_mean.mean_hessian(x, params), np.zeros((3, 3, x.shape[0])))
+    assert_allclose(linear_mean.mean_inputderiv(x, params),
+                    np.broadcast_to(np.reshape(params, (-1, 1)), (x.shape[1], x.shape[0])))
+
+def test_MeanSum(x):
+    "test the MeanSum function"
+
+    mf = FixedMean(3.) + LinearMean()
+
+    params = np.ones(3)
+
+    assert mf.get_n_params(x) == 3
+
+    assert_allclose(mf.mean_f(x, params), [9., 18.])
+    assert_allclose(mf.mean_deriv(x, params), np.transpose(x))
+    assert_allclose(mf.mean_hessian(x, params), np.zeros((3, 3, x.shape[0])))
+    assert_allclose(mf.mean_inputderiv(x, params), np.ones((x.shape[1], x.shape[0])))
+
+    mf2 = ConstantMean() + LinearMean()
+
+    params = np.ones(4)
+
+    assert mf2.get_n_params(x) == 4
+
+    assert_allclose(mf2.mean_f(x, params), [7., 16.])
+    assert_allclose(mf2.mean_deriv(x, params), np.vstack((np.ones((2,)), np.transpose(x))))
+    assert_allclose(mf2.mean_hessian(x, params), np.zeros((4, 4, x.shape[0])))
+    assert_allclose(mf2.mean_inputderiv(x, params), np.ones((x.shape[1], x.shape[0])))
+
+def test_MeanProduct(x):
+    "test the MeanProduct function"
+
+    mf = FixedMean(3.)*LinearMean()
+
+    params = np.ones(3)
+
+    assert mf.get_n_params(x) == 3
+
+    assert_allclose(mf.mean_f(x, params), [18., 45.])
+    assert_allclose(mf.mean_deriv(x, params), 3.*np.transpose(x))
+    assert_allclose(mf.mean_hessian(x, params), np.zeros((3, 3, x.shape[0])))
+    assert_allclose(mf.mean_inputderiv(x, params), 3.*np.ones((x.shape[1], x.shape[0])))
+
+    mf2 = LinearMean()*LinearMean()
+
+    params = np.ones(6)
+
+    assert mf2.get_n_params(x) == 6
+
+    hess_expected = np.zeros((6, 6, 2))
+    hess_expected[3:6, 0:3, 0] = np.array([[1., 2., 3.], [2., 4., 6.], [3., 6., 9.]])
+    hess_expected[0:3, 3:6, 0] = np.transpose([[1., 2., 3.], [2., 4., 6.], [3., 6., 9.]])
+    hess_expected[3:6, 0:3, 1] = np.array([[16., 20., 24.], [20., 25., 30.], [24., 30., 36.]])
+    hess_expected[0:3, 3:6, 1] = np.transpose([[16., 20., 24.], [20., 25., 30.], [24., 30., 36.]])
+
+    inputderiv_expected = np.zeros((3, 2))
+    inputderiv_expected[:, 0] = 12.
+    inputderiv_expected[:, 1] = 30.
+
+    assert_allclose(mf2.mean_f(x, params), [36., 225.])
+    assert_allclose(mf2.mean_deriv(x, params), np.vstack((np.transpose(x), np.transpose(x)))*np.broadcast_to([6., 15.], (6, 2)))
+    assert_allclose(mf2.mean_hessian(x, params), hess_expected)
+    assert_allclose(mf2.mean_inputderiv(x, params), inputderiv_expected)
