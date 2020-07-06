05a924133b7de4db03ac6b7d5497658987216884
edaub <edaub@turing.ac.uk>
2020-07-06 16:21:34 +0100

    modified MICE demo to match new interface

--- mogp_emulator/demos/mice_demos.py
+++ mogp_emulator/demos/mice_demos.py
@@ -1,13 +1,9 @@
 import mogp_emulator
 import numpy as np
+from projectile import simulator, print_results
 
-# simple MICE examples with 2 inputs
+# simple MICE examples using the projectile demo
 
-# simulator function -- needs to take a single input and output a single number
-
-def f(x): 
-    return np.exp(-np.sum((x-2.)**2, axis = -1)/2.)
-    
 # Base design -- requires a list of parameter bounds if you would like to use
 # uniform distributions. If you want to use different distributions, you
 # can use any of the standard distributions available in scipy to create
@@ -15,7 +11,7 @@ def f(x):
 # Internally, the code creates the design on the unit hypercube and then uses
 # the distribution to map from [0,1] to the real parameter space.
 
-ed = mogp_emulator.LatinHypercubeDesign([(0., 5.), (0., 5.)])
+lhd = mogp_emulator.LatinHypercubeDesign([(-5., 1.), (0., 1000.)])
 
 ###################################################################################
 
@@ -24,15 +20,15 @@ ed = mogp_emulator.LatinHypercubeDesign([(0., 5.), (0., 5.)])
 # first argument is base design (required), second is simulator function (optional,
 # but required if you want the code to run the simualtions internally)
 
-# Other optional arguments include: 
+# Other optional arguments include:
 # n_samples (number of sequential design steps, optional, default is not specified
 # meaning that you will specify when running the sequential design)
 # n_init (size of initial design, default 10)
 # n_cand (number of candidate points, default is 50)
 # nugget (nugget parameter for design GP, default is to set adaptively)
-# nugget_s (nugget parameter for candidate GP, default is 1.) 
+# nugget_s (nugget parameter for candidate GP, default is 1.)
 
-md = mogp_emulator.MICEDesign(ed, f, n_samples = 20, n_init = 5, n_cand = 100)
+md = mogp_emulator.MICEDesign(lhd, simulator, n_samples = 20, n_init = 5, n_cand = 100)
 
 md.run_sequential_design()
 
@@ -50,7 +46,7 @@ print()
 
 # second example: run design manually
 
-md2 = mogp_emulator.MICEDesign(ed, n_init = 5, n_cand = 100)
+md2 = mogp_emulator.MICEDesign(lhd, n_init = 5, n_cand = 100)
 
 init_design = md2.generate_initial_design()
 
@@ -59,7 +55,7 @@ print("Initial design:\n", init_design)
 
 # run initial points manually
 
-init_targets = np.array([f(s) for s in init_design])
+init_targets = np.array([simulator(s) for s in init_design])
 
 # set initial targets
 
@@ -69,9 +65,9 @@ md2.set_initial_targets(init_targets)
 
 for d in range(20):
     next_point = md2.get_next_point()
-    next_target = f(next_point)
+    next_target = simulator(next_point)
     md2.set_next_target(next_target)
-    
+
 # look at design and outputs
 
 inputs = md2.get_inputs()
@@ -80,11 +76,20 @@ targets = md2.get_targets()
 print("Final inputs:\n", inputs)
 print("Final targets:\n", targets)
 
-gp = mogp_emulator.GaussianProcess(inputs, targets)
-gp.learn_hyperparameters()
+# look at final GP emulator and make some predictions to compare with lhd
 
-testing = ed.sample(200000)
+lhd_design = lhd.sample(25)
 
-mean, unc, _ = gp.predict(testing)
+gp_lhd = mogp_emulator.fit_GP_MAP(lhd_design, np.array([simulator(p) for p in lhd_design]))
 
-print(mean)
+gp_mice = mogp_emulator.GaussianProcess(inputs, targets)
+
+gp_mice = mogp_emulator.fit_GP_MAP(inputs, targets)
+
+test_points = lhd.sample(10)
+
+print("LHD:")
+print_results(test_points, gp_lhd(test_points))
+print()
+print("MICE:")
+print_results(test_points, gp_mice(test_points))
