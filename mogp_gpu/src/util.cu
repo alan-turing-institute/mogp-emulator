cfb2921d0bff564a69b818c523c496cd4a1cd64c
nbarlowATI <nbarlow@turing.ac.uk>
2021-06-04 18:29:37 +0100

    refactor - put typedefs into types.hpp and more utility functions into utils

--- mogp_gpu/src/util.cu
+++ mogp_gpu/src/util.cu
@@ -1,3 +1,5 @@
+#include "util.hpp"
+
 // Create an NxN Identity matrix A on the device
 __global__ void identity_kernel(int N, double *A)
 {
@@ -28,4 +30,28 @@ void add_diagonal(int N, double b, double *A)
     dim3 threads_per_block(Nx, Ny);
     dim3 blocks((N + Nx - 1)/Nx, (N + Ny - 1)/Ny);
     add_diagonal_kernel<<<blocks, threads_per_block>>>(N, b, A);
+}
+
+// CUDA kernel for summing log diagonal elements of a matrix,
+// using cub::DeviceReduce
+
+struct LogSq : public thrust::unary_function<double, double>
+{
+    __host__ __device__ double operator()(double x) const { return 2.0 * log(x); }
+};
+
+void sum_log_diag(int N, double *A, double *result, double *work, size_t work_size)
+{
+    auto transform_it = thrust::make_transform_iterator(A, LogSq());
+    auto sr = make_strided_range(transform_it, transform_it + N*N, N+1);
+
+    cub::DeviceReduce::Sum(work, work_size, sr.begin(), result, N);
+}
+
+// Implementation of trace using cub::DeviceReduce
+
+void trace(int N, double *A, double *result, double *work, size_t work_size)
+{
+    auto sr = make_strided_range(A, A + N*N, N+1);
+    cub::DeviceReduce::Sum(work, work_size, sr.begin(), result, N);
 }
--- mogp_gpu/src/util.hpp
+++ mogp_gpu/src/util.hpp
@@ -1,7 +1,57 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <cusolverDn.h>
+#include <thrust/device_ptr.h>
+#include <thrust/device_malloc.h>
+#include <thrust/device_free.h>
+#include <thrust/device_vector.h>
+#include <thrust/functional.h>
+#include <thrust/transform_reduce.h>
+#include <thrust/copy.h>
+#include <cub/cub.cuh>
+
+#include <Eigen/Dense>
+
+#include "types.hpp"
+#include "strided_range.hpp"
+
+#define WARP_SIZE 32
+#define FULL_MASK 0xffffffff
+
+// ----------------------------------------------
+// ----   Utility functions ---------------------
+
+
+/// Fail if a recent cusolver call did not succeed
+inline void check_cusolver_status(cusolverStatus_t status, int info_h)
+{
+    if (status || info_h) {
+	std::string msg;
+	std::stringstream smsg(msg);
+	smsg << "Error in potrf: return code " << status << ", info " << info_h;
+	throw std::runtime_error(smsg.str());
+    }
+}
+
+/// Can a usable CUDA capable device be found?
+inline bool have_compatible_device(void)
+{
+    int device_count = 0;
+    cudaError_t err = cudaGetDeviceCount(&device_count);
+
+    return (err == cudaSuccess && device_count > 0);
+
+}
+// ----------------------------------------------
+
+
 void identity_device(int N, double *A);
 void add_diagonal(int N, double b, double *A);
+void sum_log_diag(int N, double *A, double *result, double *work, size_t work_size);
+void trace(int N, double *A, double *result, double *work, size_t work_size);
+
 
 #endif
