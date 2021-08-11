1f0c9d1336184afbdeb288516a374368028b9162
Eric Daub <edaub@turing.ac.uk>
2021-08-11 16:05:07 +0100

    basic implementation of parameters object with tests

---
+++ mogp_emulator/tests/test_GPParams.py
@@ -0,0 +1,287 @@
+import pytest
+import numpy as np
+from numpy.testing import assert_allclose
+from ..GPParams import GPParams
+
+def test_GPParams_init():
+    "Test init method of GPParams"
+    
+    gpp = GPParams()
+    
+    assert gpp.n_mean == 0
+    assert gpp.n_corr == 1
+    assert gpp.n_cov == 1
+    assert gpp.n_nugget == 1
+    assert_allclose(gpp.data, np.zeros(3))
+
+    gpp = GPParams(n_mean=2, n_corr=3, nugget=False, data=np.ones(6))
+    
+    assert gpp.n_mean == 2
+    assert gpp.n_corr == 3
+    assert gpp.n_cov == 1
+    assert gpp.n_nugget == 0
+    assert_allclose(gpp.data, np.ones(6))
+
+def test_GPParams_init_failures():
+    "Test failures of GPParams init method"
+    
+    with pytest.raises(AssertionError):
+        GPParams(n_mean=-1)
+
+    with pytest.raises(AssertionError):
+        GPParams(n_corr=-1)
+        
+    with pytest.raises(AssertionError):
+        GPParams(data=np.ones(1))
+
+def test_GPParams_n_params():
+    "Test the n_params property of GPParams"
+    
+    gpp = GPParams()
+    
+    assert gpp.n_params == 3
+    
+    gpp = GPParams(n_mean=2, n_corr=3, nugget=False)
+    
+    assert gpp.n_params == 6
+
+def test_GPParams_mean():
+    "Test the mean functionality of GPParams"
+    
+    gpp = GPParams()
+    
+    assert gpp.n_mean == 0
+    assert len(gpp.mean) == 0
+    
+    gpp.mean = []
+    
+    with pytest.raises(AssertionError):
+        gpp.mean = [1.]
+    
+    gpp = GPParams(n_mean=2, data=np.ones(5))
+    
+    assert gpp.n_mean == 2
+    assert_allclose(gpp.mean, np.ones(2))
+    assert_allclose(gpp.data[:2], np.ones(2))
+    
+    gpp.mean = np.array([2., 3.])
+    
+    assert_allclose(gpp.mean, np.array([2., 3.]))
+    assert_allclose(gpp.data[:2], np.array([2., 3.]))
+
+    with pytest.raises(AssertionError):
+        gpp.mean = []
+        
+    gpp = GPParams(n_mean=1)
+    
+    gpp.mean = 1.
+    
+    assert_allclose(gpp.mean, np.ones(1))
+
+def test_GPParams_corr():
+    "Test the correlation functionality of GPParams"
+    
+    gpp = GPParams()
+    
+    assert gpp.n_corr == 1
+    assert_allclose(gpp.corr, np.ones(1))
+    assert_allclose(gpp.corr_raw, np.zeros(1))
+
+    gpp.corr = 2.
+    
+    assert_allclose(gpp.corr, np.array([2.]))
+    assert_allclose(gpp.corr_raw, -0.5*np.log(2.))
+
+    gpp.corr = np.array([3.])
+    
+    assert_allclose(gpp.corr, np.array([3.]))
+    assert_allclose(gpp.corr_raw, -0.5*np.log(3.))
+    
+    gpp.corr_raw = 0.
+    
+    assert_allclose(gpp.corr, np.ones(1))
+    assert_allclose(gpp.corr_raw, np.zeros(1))
+    
+    gpp.corr_raw = np.array([3.])
+    
+    assert_allclose(gpp.corr, np.exp(-2.*np.array([3.])))
+    assert_allclose(gpp.corr_raw, 3.)
+
+    with pytest.raises(AssertionError):
+        gpp.corr = np.array([2., 3.])
+    
+    with pytest.raises(AssertionError):
+        gpp.corr_raw = np.array([2., 3.])
+        
+    with pytest.raises(AssertionError):
+        gpp.corr = -1.
+        
+    gpp = GPParams(n_corr=3, data=np.array([2., 3., 2., 0., 0.]))
+    
+    assert gpp.n_corr == 3
+    assert_allclose(gpp.corr, np.exp(-2.*np.array([2., 3., 2.])))
+    assert_allclose(gpp.corr_raw, np.array([2., 3., 2.]))
+
+    gpp.corr = np.ones(3)
+    
+    assert_allclose(gpp.corr, np.ones(3))
+    assert_allclose(gpp.corr_raw, np.zeros(3))
+    
+    gpp.corr_raw = np.ones(3)
+    
+    assert_allclose(gpp.corr, np.exp(-2.*np.ones(3)))
+    assert_allclose(gpp.corr_raw, np.ones(3))
+
+    with pytest.raises(AssertionError):
+        gpp.corr = np.array([2., 3.])
+
+    with pytest.raises(AssertionError):
+        gpp.corr_raw = np.array([2., 3.])
+        
+    with pytest.raises(AssertionError):
+        gpp.corr = np.array([-1., 3., 3.])
+
+def test_GPParams_cov():
+    "Test the covariance functionality of GPParams"
+    
+    gpp = GPParams()
+    
+    assert gpp.n_cov == 1
+    assert_allclose(gpp.cov, np.ones(1))
+    assert_allclose(gpp.cov_raw, np.zeros(1))
+
+    gpp.cov = 2.
+    
+    assert_allclose(gpp.cov, np.array([2.]))
+    assert_allclose(gpp.cov_raw, 0.5*np.log(2.))
+
+    gpp.cov = np.array([3.])
+    
+    assert_allclose(gpp.cov, np.array([3.]))
+    assert_allclose(gpp.cov_raw, 0.5*np.log(3.))
+    
+    gpp.cov_raw = 0.
+    
+    assert_allclose(gpp.cov, np.ones(1))
+    assert_allclose(gpp.cov_raw, np.zeros(1))
+    
+    gpp.cov_raw = np.array([3.])
+    
+    assert_allclose(gpp.cov, np.exp(2.*np.array([3.])))
+    assert_allclose(gpp.cov_raw, 3.)
+
+    with pytest.raises(AssertionError):
+        gpp.cov = np.array([2., 3.])
+    
+    with pytest.raises(AssertionError):
+        gpp.cov_raw = np.array([2., 3.])
+        
+    with pytest.raises(AssertionError):
+        gpp.cov = -1.
+        
+    gpp = GPParams(data=np.array([0., 2., 0.]))
+    
+    assert gpp.n_cov == 1
+    assert_allclose(gpp.cov, np.exp(2.*np.array([2.])))
+    assert_allclose(gpp.cov_raw, np.array([2.]))
+
+    gpp.cov = np.ones(1)
+    
+    assert_allclose(gpp.cov, np.ones(1))
+    assert_allclose(gpp.cov_raw, np.zeros(1))
+    
+    gpp.cov = 1.
+    
+    assert_allclose(gpp.cov, np.ones(1))
+    assert_allclose(gpp.cov_raw, np.zeros(1))
+    
+    gpp.cov_raw = np.ones(1)
+    
+    assert_allclose(gpp.cov, np.exp(2.*np.ones(1)))
+    assert_allclose(gpp.cov_raw, np.ones(1))
+
+    with pytest.raises(AssertionError):
+        gpp.cov = np.array([2., 3.])
+
+    with pytest.raises(AssertionError):
+        gpp.cov_raw = np.array([2., 3.])
+        
+    with pytest.raises(AssertionError):
+        gpp.cov = np.array([-1.])
+
+def test_GPParams_nugget():
+    "Test the covariance functionality of GPParams"
+    
+    gpp = GPParams()
+    
+    assert gpp.n_nugget == 1
+    assert_allclose(gpp.nugget, np.ones(1))
+    assert_allclose(gpp.nugget_raw, np.zeros(1))
+
+    gpp.nugget = 2.
+    
+    assert_allclose(gpp.nugget, np.array([2.]))
+    assert_allclose(gpp.nugget_raw, 0.5*np.log(2.))
+
+    gpp.nugget = np.array([3.])
+    
+    assert_allclose(gpp.nugget, np.array([3.]))
+    assert_allclose(gpp.nugget_raw, 0.5*np.log(3.))
+    
+    gpp.nugget_raw = 0.
+    
+    assert_allclose(gpp.nugget, np.ones(1))
+    assert_allclose(gpp.nugget_raw, np.zeros(1))
+    
+    gpp.nugget_raw = np.array([3.])
+    
+    assert_allclose(gpp.nugget, np.exp(2.*np.array([3.])))
+    assert_allclose(gpp.nugget_raw, 3.)
+
+    with pytest.raises(AssertionError):
+        gpp.nugget = np.array([2., 3.])
+    
+    with pytest.raises(AssertionError):
+        gpp.nugget_raw = np.array([2., 3.])
+        
+    with pytest.raises(AssertionError):
+        gpp.nugget = -1.
+        
+    gpp = GPParams(data=np.array([0., 0., 2.]))
+    
+    assert gpp.n_nugget == 1
+    assert_allclose(gpp.nugget, np.exp(2.*np.array([2.])))
+    assert_allclose(gpp.nugget_raw, np.array([2.]))
+
+    gpp = GPParams(nugget=False)
+    
+    assert gpp.n_nugget == 0
+    assert len(gpp.nugget) == 0
+    assert len(gpp.nugget_raw) == 0
+
+def test_GPParams_data():
+    "Test the data getter and setter methods"
+    
+    gpp = GPParams()
+    
+    assert_allclose(gpp.get_data(), np.zeros(3))
+    
+    gpp.set_data(np.ones(3))
+    
+    assert_allclose(gpp.data, np.ones(3))
+    
+    with pytest.raises(AssertionError):
+        gpp.set_data(np.ones(4))
+        
+    gpp = GPParams(n_mean=2, n_corr=3)
+    
+    assert_allclose(gpp.get_data(), np.zeros(7))
+    
+    gpp.set_data(np.ones(7))
+    
+    assert_allclose(gpp.data, np.ones(7))
+    
+def test_GPParams_str():
+    "Test the str method of GPParams doesn't raise an error"
+    
+    str(GPParams())
