d42b003089cc60d0e3b1726e025c79ef8882d8e9
Oliver Strickson <ostrickson@turing.ac.uk>
2022-09-05 16:15:27 +0100

    Use cholesky/cho_solve instead of general linear solver
    - Possible since the regularized Gram matrix is Hermitian
    
    - Use some more descriptive variable names

--- mogp_emulator/DimensionReduction.py
+++ mogp_emulator/DimensionReduction.py
@@ -71,6 +71,7 @@ regression on the reduced input space:
 import sys
 import numpy as np
 from scipy.spatial.distance import cdist, pdist, squareform
+from scipy.linalg import cho_solve, cholesky
 from mogp_emulator.utils import k_fold_cross_validation, integer_bisect
 
 def gram_matrix(X, k):
@@ -199,8 +200,10 @@ class gKDR(object):
         Kx = gram_matrix_sqexp(X, SGX2)
         Ky = gram_matrix_sqexp(Y, SGY2)
 
-        tmp = np.linalg.solve(Kx + N*EPS*I, Ky)
-        F = np.linalg.solve((Kx + N*EPS*I).T, tmp.T).T
+        regularized_Kx = Kx + N*EPS*I
+        lower_chol_factor = cholesky(regularized_Kx, lower=True)
+        tmp = cho_solve((lower_chol_factor, True), Ky)
+        F = cho_solve((lower_chol_factor, True), tmp.T).T
 
         Dx = np.reshape(np.tile(X,(N,1)), (N,N,M), order='F').copy()
         Xij = Dx - np.transpose(Dx, (1,0,2))
